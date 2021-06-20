5a8a9179f1c937c64794299c735c331f5a401687
Michael Hanna <michael.ramzy@gmail.com>
2021-06-20 19:20:34 +1000

    Adding unit tests

---
+++ PasswordChecker/PasswordChecker/PasswordChecker.Test/PasswordCheckerUnitTest.cs
@@ -0,0 +1,108 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordChecker.Models;
+
+namespace PasswordChecker.Test
+{
+    [TestClass]
+    public class PasswordCheckerUnitTest
+    {
+        [TestMethod]
+        public void TestBlankPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword(string.Empty);
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.NoComply, Message = string.Format(Constants.MESSAGE_SHORT_PASSWORD, Constants.MINIMUM_LENGTH), BreachCount = 0 });
+        }
+
+        [TestMethod]
+        public void TestShortPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pass1");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.NoComply, Message = string.Format(Constants.MESSAGE_SHORT_PASSWORD, Constants.MINIMUM_LENGTH), BreachCount = 0 });
+        }
+
+        [TestMethod]
+        public void TestLowerCasePassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("password");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.NoComply, Message = Constants.MESSAGE_NO_CAPITAL_LETTERS, BreachCount = 0 });
+        }
+
+        [TestMethod]
+        public void TestUpperCasePassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("PASSWORD");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.NoComply, Message = Constants.MESSAGE_NO_SMALL_LETTERS, BreachCount = 0 });
+        }
+
+        [TestMethod]
+        public void TestUpperLowerCasePassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Password");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.NoComply, Message = Constants.MESSAGE_NO_NUMBER, BreachCount = 0 });
+        }
+
+        [TestMethod]
+        public void TestVeryWeakUnbreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("psdD23");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.VeryWeak, Message = string.Empty, BreachCount = 0 });
+        }
+        [TestMethod]
+        public void TestVeryWeakBreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pass12");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.VeryWeak, Message = string.Empty, BreachCount = 354 });
+        }
+
+        [TestMethod]
+        public void TestWeakUnbreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("pAsS123");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.Weak, Message = string.Empty, BreachCount = 0 });
+        }
+        [TestMethod]
+        public void TestWeakBreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pass123");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.Weak, Message = string.Empty, BreachCount = 3981 });
+        }
+
+        [TestMethod]
+        public void TestMediumUnbreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pass$12");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.Medium, Message = string.Empty, BreachCount = 0 });
+        }
+        [TestMethod]
+        public void TestMediumBreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pa$$1234");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.Medium, Message = string.Empty, BreachCount = 33 });
+        }
+        [TestMethod]
+        public void TestStrongUnbreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pas$93453");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.Strong, Message = string.Empty, BreachCount = 0 });
+        }
+        [TestMethod]
+        public void TestStrongBreachedPassword()
+        {
+            var apiService = new PasswordCheckerService();
+            var result = apiService.CheckPassword("Pa$$w0rd1");
+            Assert.AreEqual(result, new PasswordCheckResult() { PasswordStrength = Constants.PasswordStrength.Strong, Message = string.Empty, BreachCount = 327 });
+        }
+    }
+}
--- PasswordChecker/PasswordChecker/PasswordChecker/Models/PasswordCheckResult.cs
+++ PasswordChecker/PasswordChecker/PasswordChecker/Models/PasswordCheckResult.cs
@@ -8,5 +8,15 @@ namespace PasswordChecker.Models
         public PasswordStrength PasswordStrength { get; set; }
         public string Message { get; set; }
         public int BreachCount { get; set; }
+
+        public PasswordCheckResult()
+        {
+            Message = string.Empty;
+        }
+        public override bool Equals(object obj)
+        {
+            var otherObject = obj as PasswordCheckResult;
+            return Message.Equals(otherObject.Message) && PasswordStrength == otherObject.PasswordStrength && BreachCount == otherObject.BreachCount;
+        }
     }
 }
